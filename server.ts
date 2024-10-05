842ec7b5972c0ab860e40de2d685b21dea6edd5e
“[firstname <dafne.faviola.luna@gmail.com>
2024-10-05 10:05:09 -0400

    starter code and file outlines

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+// import routes from './routes/index.js';
+// import db from './config/connection.js';
+
+const startServer = async () => {
+    await db();
+
+    const PORT = process.env.PORT || 3001;
+    const app = express();
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    app.use(routes);
+
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+};
+
+startServer();
